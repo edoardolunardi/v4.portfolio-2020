36d02685200054455b0cf8d0a95f38e389e2b8c8
Edoardo <edward.lunardi@gmail.com>
2020-08-15 14:16:20 +0200

    Added FG font

--- src/styles/Global.js
+++ src/styles/Global.js
@@ -1,8 +1,16 @@
 import { createGlobalStyle } from "styled-components"
+import FG from "../fonts/FG-medium.woff"
 
 export default createGlobalStyle`
+	@font-face {
+		font-family: FG;
+		src: url(${FG}) format('woff');
+		font-weight: normal;
+		font-style: normal;
+	}
+
 	html {
-    font-family: sans-serif;
+		font-family: FG, sans-serif;
     -ms-text-size-adjust: 100%;
     -webkit-text-size-adjust: 100%;
 	}
