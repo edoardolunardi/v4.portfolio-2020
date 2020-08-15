5f47731a882967d2a0d7f657bbc0f3b0c3a57814
Edoardo <edward.lunardi@gmail.com>
2020-08-15 14:32:36 +0200

    styled.createGlobalStyle so Prettier can format it

--- src/styles/Global.js
+++ src/styles/Global.js
@@ -1,30 +1,35 @@
-import { createGlobalStyle } from "styled-components"
+import { createGlobalStyle, styled } from "styled-components"
 import { normalize } from "styled-normalize"
 import FG from "../fonts/FG-medium.woff"
 
-export default createGlobalStyle`
-	@font-face {
-		font-family: FG;
-		src: url(${FG}) format('woff');
-		font-weight: normal;
-		font-style: normal;
-	}
+export default styled.createGlobalStyle`
+  @font-face {
+    font-family: FG;
+    src: url(${FG}) format("woff");
+    font-weight: normal;
+    font-style: normal;
+  }
 
-	${normalize}
+  ${normalize}
 
-	html {
-		font-family: FG, sans-serif;
+  html {
+    font-family: FG, sans-serif;
     -ms-text-size-adjust: 100%;
     -webkit-text-size-adjust: 100%;
-	}
-	
+  }
+
   body {
-		padding: 2rem;
+    padding: 2rem;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-	}
-	
-	h1, h2, h3, h4, h5, h6{
-		margin: 0
-	}
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin: 0;
+  }
 `
