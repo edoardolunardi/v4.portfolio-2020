b46363cb0b3b75380a1962e7cfdc5065abb9fe7c
Edoardo <edward.lunardi@gmail.com>
2020-08-27 22:33:28 +0200

    Rm current at wild in project

--- src/pages/project/diego.js
+++ src/pages/project/diego.js
@@ -114,8 +114,6 @@ const DiegoPage = () => {
                     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
                     "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut",
                     "enim ad minim veniam, quis nostrud exercitation",
-                    " ",
-                    "Currently at WILD",
                   ]}
                 />
               </Paragraph>
